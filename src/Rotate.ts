d6df5e77906dbc24821e91a1d106ebb8d50371f8
Yehor <20585619+satelllte@users.noreply.github.com>
2024-04-13 18:15:34 +0300

    CharEncoder

--- src/Rotate.ts
+++ src/Rotate.ts
@@ -35,6 +35,7 @@ import {ROTATE_Manager} from './ROTATE_Manager';
 import {Time} from './Time';
 import {LocalStorage} from './LocalStorage';
 import {StringBytesObject} from './StringBytesObject';
+import {CharEncoder} from './CharEncoder';
 
 // ---------------------------------------------------------------------------
 
@@ -1075,79 +1076,6 @@ CharUtils.decode = function (a: string, b: boolean = true) {
   return new CharEncoder(CharUtils.BYTES).decodeBytes(
     StringBytesObject.ofString(a),
   );
-};
-
-var CharEncoder = function (a) {
-  for (var b = a.length, c = 1; b > 1 << c; ) ++c;
-  if (8 < c || b != 1 << c)
-    throw new ROTATE_Error('BaseCode : base length must be a power of two.');
-  this.base = a;
-  this.nbits = c;
-};
-CharEncoder.__name__ = !0;
-CharEncoder.prototype = {
-  encodeBytes: function (a) {
-    for (
-      var b = this.nbits,
-        c = this.base,
-        d = ((8 * a.length) / b) | 0,
-        e = new StringBytesObject(
-          new ArrayBuffer(d + (0 == (8 * a.length) % b ? 0 : 1)),
-        ),
-        f = 0,
-        m = 0,
-        k = (1 << b) - 1,
-        p = 0,
-        y = 0;
-      y < d;
-
-    ) {
-      for (; m < b; ) (m += 8), (f <<= 8), (f |= a.b[p++]);
-      m -= b;
-      e.b[y++] = c.b[(f >> m) & k] & 255;
-    }
-    0 < m && (e.b[y++] = c.b[(f << (b - m)) & k] & 255);
-    return e;
-  },
-  initTable: function () {
-    for (var a = [], b = 0; 256 > b; ) {
-      var c = b++;
-      a[c] = -1;
-    }
-    b = 0;
-    for (c = this.base.length; b < c; ) {
-      var d = b++;
-      a[this.base.b[d]] = d;
-    }
-    this.tbl = a;
-  },
-  decodeBytes: function (a) {
-    var b = this.nbits;
-    null == this.tbl && this.initTable();
-    for (
-      var c = this.tbl,
-        d = (a.length * b) >> 3,
-        e = new StringBytesObject(new ArrayBuffer(d)),
-        f = 0,
-        m = 0,
-        k = 0,
-        p = 0;
-      p < d;
-
-    ) {
-      for (; 8 > m; ) {
-        m += b;
-        f <<= b;
-        var y = c[a.b[k++]];
-        if (-1 == y) throw new ROTATE_Error('BaseCode : invalid encoded char');
-        f |= y;
-      }
-      m -= 8;
-      e.b[p++] = (f >> m) & 255;
-    }
-    return e;
-  },
-  __class__: CharEncoder,
 };
 
 var ROTATE_KeysMap = function () {
