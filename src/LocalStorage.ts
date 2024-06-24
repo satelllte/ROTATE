0b40246abf876a368eebf942afc3ac70661aec1f
Yehor <20585619+satelllte@users.noreply.github.com>
2024-06-24 22:26:02 +0300

    LocalStorage: consistent impl

--- src/LocalStorage.ts
+++ src/LocalStorage.ts
@@ -1,13 +1,13 @@
 export class LocalStorage {
-  public static getItem = function (key: string) {
+  public static getItem(key: string) {
     return window.localStorage.getItem(key);
-  };
+  }
 
   public static setItem(key: string, value: string) {
     window.localStorage.setItem(key, value);
   }
 
-  public static removeItem = function (key: string) {
+  public static removeItem(key: string) {
     window.localStorage.removeItem(key);
-  };
+  }
 }
