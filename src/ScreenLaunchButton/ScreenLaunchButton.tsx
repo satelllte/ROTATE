bdf891069d4e669e73723294b02cd7dc0f9ce481
Yehor <20585619+satelllte@users.noreply.github.com>
2024-08-10 20:03:59 +0300

    ScreenLaunchButton: more functions split

--- src/ScreenLaunchButton/ScreenLaunchButton.tsx
+++ src/ScreenLaunchButton/ScreenLaunchButton.tsx
@@ -46,37 +46,40 @@ function Interaction({
   readonly image: HTMLImageElement;
 }) {
   useEffect(() => {
-    const getXYOnCanvas = (event: PointerEvent) => {
-      const canvasRect = ctx.canvas.getBoundingClientRect();
-      const x = event.x - canvasRect.x;
-      const y = event.y - canvasRect.y;
-      return {x, y};
-    };
-
-    const calcIsOverButton = (
-      image: HTMLImageElement,
-      x: number,
-      y: number,
-    ): boolean => {
-      const xMin = ctx.canvas.width * 0.5 - image.width * 0.5;
-      const xMax = xMin + image.width;
-      const yMin = ctx.canvas.height * 0.5 - image.height * 0.5;
-      const yMax = yMin + image.height;
-      return x >= xMin && x <= xMax && y >= yMin && y <= yMax;
-    };
+    const {canvas} = ctx;
 
     const onPointerMove = (event: PointerEvent) => {
-      const {x, y} = getXYOnCanvas(event);
-      const isOverButton = calcIsOverButton(image, x, y);
+      const {x, y} = getXYOnCanvas(canvas, event);
+      const isOverButton = calcIsOverButton(canvas, image, x, y);
       console.debug('[onPointerMove] isOverButton: ', isOverButton);
     };
 
-    ctx.canvas.addEventListener('pointermove', onPointerMove);
+    canvas.addEventListener('pointermove', onPointerMove);
 
     return () => {
-      ctx.canvas.removeEventListener('pointermove', onPointerMove);
+      canvas.removeEventListener('pointermove', onPointerMove);
     };
   }, [ctx, image]);
 
   return null;
 }
+
+const getXYOnCanvas = (canvas: HTMLCanvasElement, event: PointerEvent) => {
+  const canvasRect = canvas.getBoundingClientRect();
+  const x = event.x - canvasRect.x;
+  const y = event.y - canvasRect.y;
+  return {x, y};
+};
+
+const calcIsOverButton = (
+  canvas: HTMLCanvasElement,
+  image: HTMLImageElement,
+  x: number,
+  y: number,
+): boolean => {
+  const xMin = canvas.width * 0.5 - image.width * 0.5;
+  const xMax = xMin + image.width;
+  const yMin = canvas.height * 0.5 - image.height * 0.5;
+  const yMax = yMin + image.height;
+  return x >= xMin && x <= xMax && y >= yMin && y <= yMax;
+};
